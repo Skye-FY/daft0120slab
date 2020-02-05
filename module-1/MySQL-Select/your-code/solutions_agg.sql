cc5a6f1e23dfec2df5035adc382a45b67ec55ec0
Skye-FY <[50981451+Skye-FY@users.noreply.github.com]>
2020-02-05 11:24:19 -0500

    .

--- module-1/MySQL-Select/your-code/solutions_agg.sql
+++ module-1/MySQL-Select/your-code/solutions_agg.sql
@@ -15,15 +15,15 @@ ORDER BY type_cnt DESC;
 
 -- 3. Using the loan table, print the top 10 account_ids based on the sum of all of their loan amounts.
 CREATE VIEW top10_loanid AS
-SELECT loan.account_id, loan.amount
+SELECT loan.account_id, SUM(loan.amount) AS amt_sum
 FROM bank.loan
 GROUP BY loan.account_id
-ORDER BY loan.amount DESC
+ORDER BY amt_sum DESC
 LIMIT 10;
 
 -- 4. From the loan table, retrieve the number of loans issued for each day, before (excl) 930907, ordered by date in descending order
 CREATE VIEW loan_issued AS
-SELECT loan.`date`, COUNT(loan.`date`) AS issued
+SELECT loan.`date`, COUNT(*) AS issued
 FROM bank.loan
 WHERE loan.`date` < 930907
 GROUP BY loan.`date`
@@ -33,6 +33,9 @@ ORDER BY loan.`date` DESC;
 CREATE VIEW issue_tims AS
 SELECT loan.`date`, loan.duration, COUNT(loan.`duration`) AS issued
 FROM bank.loan
+WHERE 
+	loan.`date` >= 971201
+    AND loan.`date` <980101
 GROUP BY loan.`date`
 ORDER BY loan.`date`, loan.duration ASC;
 
@@ -40,13 +43,22 @@ ORDER BY loan.`date`, loan.duration ASC;
 CREATE VIEW sum396 AS
 SELECT trans.account_id, trans.`type`, SUM(trans.amount) AS total_amount
 FROM bank.trans
-WHERE trans.account_id = 396
+WHERE trans.account_id = 396 
+GROUP BY trans.account_id, trans.`type`
 ORDER BY trans.`type` ASC;
 
 -- 7. From the previous output, translate the values for type to English, rename the column to transaction_type, round total_amount down to an integer
 -- UPDATE bank.trans
 -- SET trans.`type` = 'OUTGOING'
 -- WHERE trans.account_id = 396;
+SELECT 
+	trans.account_id,
+    IF(type = 'PRIJEM', 'INCOMING', 'OUTGOING') AS transction_type,
+    FLOOR(SUM(amount)) AS total_amount
+FROM bank.trans
+WHERE trans.account_id = 396
+GROUP BY trans.account_id, type
+ORDER BY 3
 
 -- 8. From the previous result, modify you query so that it returns only one row, with a column for incoming amount, outgoing amount and the difference
 
